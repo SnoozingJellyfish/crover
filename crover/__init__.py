841f250cfa4d1be2d453fd83880ad74ab6914f2f
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2021-03-30 11:54:38 +0900

    load from google cloud storage

--- crover/__init__.py
+++ crover/__init__.py
@@ -2,6 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+import pickle
+import BytesIO
+
+import logging
+# フォーマットを定義
+formatter = '%(levelname)s : %(asctime)s : %(message)s'
+
+import cloudstorage
+retryparams_instance = cloudstorage.RetryParams(initial_delay=0.2, max_delay=5.0, backoff_factor=2, max_retry_period=15)
+cloudstorage.set_default_retry_params(retryparams_instance)
+dict_all_count_obj = cloudstorage.open(filename='/crover_word2vec/all_1-200-000_word_count_sudachi.pickle', mode='rb', retry_params=retryparams_instance)
+dict_all_count = pickle.load(BytesIO(dict_all_count_obj))
+dict_all_count_obj.close()
+print(type(dict_all_count))
+
+# ログレベルを DEBUG に変更
+logging.basicConfig(level=logging.INFO, format=formatter)
+
+'''
+with open('crover/data/all_1-200-000_word_count_sudachi.pickle', 'rb') as f:
+    dict_all_count = pickle.load(f)
+
+with open('crover/data/word_id.pickle', 'rb') as f:
+    word_id = pickle.load(f)
+'''
+
 def create_app(test_config=None):
     app = Flask(__name__, static_folder='figure')
     app.config.from_object('crover.config')
