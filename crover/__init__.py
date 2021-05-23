8cef320e12a18590d82f06e7afbd2811054d1db3
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2021-05-23 22:01:08 +0900

    dummy

--- crover/__init__.py
+++ crover/__init__.py
@@ -78,6 +78,7 @@ else:
             }
         )
     )
+    
     db_session = scoped_session(sessionmaker(bind=engine))
     Base = declarative_base()
     Base.query = db_session.query_property()
