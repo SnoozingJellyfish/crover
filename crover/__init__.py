5d9c816879dbb299322f114a977d4869d0ee407f
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2021-05-24 23:46:06 +0900

    mb4

--- crover/__init__.py
+++ crover/__init__.py
@@ -76,11 +76,10 @@ else:
                 "unix_socket": "{}/{}".format(
                     db_socket_dir,  # e.g. "/cloudsql"
                     cloud_sql_connection_name),  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-                'charset': 'utf8'
+                'charset': 'utf8mb4'
             }
 
-        ),
-        encoding="utf8"
+        )
     )
 
     db_session = scoped_session(sessionmaker(bind=engine))
