320179a93ea78335efe3ffbf5e0ea8e74e45a2fc
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2022-02-06 18:23:06 +0900

    spinner関数の位置を元に戻した

---
+++ crover/static/spinner.js
@@ -0,0 +1,26 @@
+// イベントを禁止する関数
+function noScroll(event) {
+    event.preventDefault();
+}
+
+// スピナーを有効にしスクロールを禁止する
+function enable_spinner() {
+    console.log('enable spinner');
+    document.getElementsByClassName("loader")[0].style.display = "block";
+
+    // スクロール禁止(SP)
+    document.addEventListener('touchmove', noScroll, { passive: false });
+    // スクロール禁止(PC)
+    document.addEventListener('mousewheel', noScroll, { passive: false });
+}
+
+// スピナーを無効にしスクロールを解除する
+function disable_spinner() {
+    console.log('disable spinner');
+    document.getElementsByClassName("loader")[0].style.display = "none";
+
+    // スクロール禁止を解除(SP)
+    document.removeEventListener('touchmove', noScroll, { passive: false });
+    // スクロール禁止を解除(PC)
+    document.removeEventListener('mousewheel', noScroll, { passive: false });
+}
